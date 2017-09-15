5251f9d2cecbe0eeb94ed7b0392e4ce3b74d5a30
Unknown <zacharyd1@live.com>
2017-09-15 10:26:52 -0400

    Added given source code

---
+++ Lab3/convert.cpp
@@ -0,0 +1,21 @@
+// Program Convert converts a temperature from Fahrenheit to 
+// Celsius and a temperature from Celsius to Fahrenheit.
+#include <iostream>
+using namespace std;
+
+const int TEMP_IN_F = 32;  
+const int TEMP_IN_C = 0;
+
+int main ()
+{
+  int fToC;	// Place to store Celsius answer
+  int cToF;	// Place to store Fahrenheit answer 
+
+  cToF = (9 * TEMP_IN_C / 5) + 32;                      
+  fToC = 5 * (TEMP_IN_F - 32) / 9; 
+  cout << TEMP_IN_F  << " in Fahrenheit is " << fToC << " in Celsius. "  << endl; 
+  
+
+  cin.ignore();
+  return 0;
+}
