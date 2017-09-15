e9e4bce65f08b66fee68ed5244d78ed00a9acaad
zachary-D <zacharyd1@live.com>
2017-09-15 11:53:09 -0400

    Exercise 3

--- Lab3/Lesson3-3.cpp
+++ Lab3/Lesson3-3.cpp
@@ -20,6 +20,11 @@ void printRight(int value, int width = 10)
 	cout << setw(width) << value << endl;
 }
 
+void printPrecision(float value, int precision = 4)
+{
+	cout << setprecision(precision) << value << endl;
+}
+
 
 int main()
 {
@@ -31,7 +36,19 @@ int main()
 
 	cout << "The answer is:" << setprecision(5) << (float)(first + second) / (float)(third + fourth + fifth) << endl;
 
+	{//To get ourselves a new 'locality' so we can redefine first, second, etc. without issue
+		const float first = 23.62;
+		const float second = 46.0;
+		const float third = 43.4443;
+		const float fourth = 100.1;
+		const float fifth = 98.98;
+		printPrecision(first, 4);
+		printPrecision(second, 4);
+		printPrecision(third, 4);
+		printPrecision(fourth, 5);
+		printPrecision(fifth, 5);
 
+	}
 
 
 	cin.get();
