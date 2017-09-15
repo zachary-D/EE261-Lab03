a5e714e29d25ba8e687c01d57ea8d807fb78d040
Unknown <zacharyd1@live.com>
2017-09-15 10:27:34 -0400

    Exercise 2

--- Lab3/convert.cpp
+++ Lab3/convert.cpp
@@ -14,7 +14,7 @@ int main ()
   cToF = (9 * TEMP_IN_C / 5) + 32;                      
   fToC = 5 * (TEMP_IN_F - 32) / 9; 
   cout << TEMP_IN_F  << " in Fahrenheit is " << fToC << " in Celsius. "  << endl; 
-  
+  cout << TEMP_IN_C << " in Celcius is " << cToF << " in Fahrenheit" << endl;
 
   cin.ignore();
   return 0;
